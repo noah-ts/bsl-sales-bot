c4e4777b82984afaedacf2505ee0d3ab691cd143
Noyan <noyan.alimov@gmail.com>
2022-09-07 00:51:35 +0100

    every 2 minutes and inside while loop

--- dist/index.js
+++ dist/index.js
@@ -8,26 +8,38 @@ const API_KEY = process.env.HELIUS_API_KEY;
 const address = '8m2b8ar9BNZErJQgSBwY3eCe73yR4k9qHUxxGffxyw2d';
 const LAMPORTS_PER_SOL = 1000000000;
 let mostRecentTxn = '';
+let interval;
 console.log('Server started');
-const interval = setInterval(async () => {
-    const res = await fetch(`https://api.helius.xyz/v0/addresses/${address}/nft-events?type=NFT_SALE&api-key=${API_KEY}&until=${mostRecentTxn}`);
-    const data = await res.json();
-    if (!data.length)
-        return;
-    for (let i = data.length - 1; i >= 0; i--) {
-        const event = data[i];
-        const n = await prisma.nft.update({
-            where: { mint: event.nfts[0].mint },
-            data: {
-                lastSaleDate: dayjs.unix(event.timestamp).format(),
-                lastSaleAmountSol: Number(event.amount) / LAMPORTS_PER_SOL
+const salesBot = () => {
+    while (true) {
+        interval = setInterval(async () => {
+            try {
+                const res = await fetch(`https://api.helius.xyz/v0/addresses/${address}/nft-events?type=NFT_SALE&api-key=${API_KEY}&until=${mostRecentTxn}`);
+                const data = await res.json();
+                if (!data.length)
+                    return;
+                for (let i = data.length - 1; i >= 0; i--) {
+                    const event = data[i];
+                    const n = await prisma.nft.update({
+                        where: { mint: event.nfts[0].mint },
+                        data: {
+                            lastSaleDate: dayjs.unix(event.timestamp).format(),
+                            lastSaleAmountSol: Number(event.amount) / LAMPORTS_PER_SOL
+                        }
+                    });
+                    console.log(n);
+                }
+                mostRecentTxn = data[0].signature;
             }
-        });
-        console.log(n);
+            catch (err) {
+                console.error('Error fetching from helius: ', err);
+                return;
+            }
+            // 2 minutes
+        }, 120000);
     }
-    mostRecentTxn = data[0].signature;
-    // 10 seconds
-}, 10000);
+};
+salesBot();
 process.on('uncaughtException', async (err) => {
     console.error('Uncaught Exception: ', err);
     console.log('Most Recent Transaction: ', mostRecentTxn);
