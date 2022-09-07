472b62000ac8e76d45772412d966494613aa7aa1
Noyan <noyan.alimov@gmail.com>
2022-09-07 14:55:29 +0100

    stop sales bot

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import dayjs from 'dayjs';
-import fetch from 'node-fetch';
-import { setTimeout } from 'timers/promises';
 import http from 'http';
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -11,35 +8,33 @@ const address = '8m2b8ar9BNZErJQgSBwY3eCe73yR4k9qHUxxGffxyw2d';
 const LAMPORTS_PER_SOL = 1000000000;
 let mostRecentTxn = '';
 console.log('Server started');
-const salesBot = async () => {
-    while (true) {
-        try {
-            // 2 minutes
-            await setTimeout(120000);
-            const res = await fetch(`https://api.helius.xyz/v0/addresses/${address}/nft-events?type=NFT_SALE&api-key=${API_KEY}&until=${mostRecentTxn}`);
-            const data = await res.json();
-            if (!data.length)
-                continue;
-            for (let i = data.length - 1; i >= 0; i--) {
-                const event = data[i];
-                const n = await prisma.nft.update({
-                    where: { mint: event.nfts[0].mint },
-                    data: {
-                        lastSaleDate: dayjs.unix(event.timestamp).format(),
-                        lastSaleAmountSol: Number(event.amount) / LAMPORTS_PER_SOL
-                    }
-                });
-                console.log(n);
-            }
-            mostRecentTxn = data[0].signature;
-        }
-        catch (err) {
-            console.error('Error fetching from helius: ', err);
-            continue;
-        }
-    }
-};
-salesBot();
+// const salesBot = async () => {
+//   while (true) {
+//     try {
+//       // 2 minutes
+//       await setTimeout(120000)
+//       const res = await fetch(`https://api.helius.xyz/v0/addresses/${address}/nft-events?type=NFT_SALE&api-key=${API_KEY}&until=${mostRecentTxn}`)
+//       const data = await res.json() as HeliusNftEventResponseType[]
+//       if (!data.length) continue
+//       for (let i = data.length - 1; i >= 0; i--) {
+//         const event = data[i]
+//         const n = await prisma.nft.update({
+//           where: { mint: event.nfts[0].mint },
+//           data: {
+//             lastSaleDate: dayjs.unix(event.timestamp).format(),
+//             lastSaleAmountSol: Number(event.amount) / LAMPORTS_PER_SOL
+//           }
+//         })
+//         console.log(n)
+//       }
+//       mostRecentTxn = data[0].signature
+//     } catch (err) {
+//       console.error('Error fetching from helius: ', err)
+//       continue
+//     }
+//   }
+// }
+// salesBot()
 process.on('uncaughtException', async (err) => {
     console.error('Uncaught Exception: ', err);
     console.log('Most Recent Transaction: ', mostRecentTxn);
--- src/index.ts
+++ src/index.ts
@@ -17,36 +17,36 @@ let mostRecentTxn = ''
 
 console.log('Server started')
 
-const salesBot = async () => {
-  while (true) {
-    try {
-      // 2 minutes
-      await setTimeout(120000)
-      const res = await fetch(`https://api.helius.xyz/v0/addresses/${address}/nft-events?type=NFT_SALE&api-key=${API_KEY}&until=${mostRecentTxn}`)
-      const data = await res.json() as HeliusNftEventResponseType[]
-      if (!data.length) continue
+// const salesBot = async () => {
+//   while (true) {
+//     try {
+//       // 2 minutes
+//       await setTimeout(120000)
+//       const res = await fetch(`https://api.helius.xyz/v0/addresses/${address}/nft-events?type=NFT_SALE&api-key=${API_KEY}&until=${mostRecentTxn}`)
+//       const data = await res.json() as HeliusNftEventResponseType[]
+//       if (!data.length) continue
     
-      for (let i = data.length - 1; i >= 0; i--) {
-        const event = data[i]
-        const n = await prisma.nft.update({
-          where: { mint: event.nfts[0].mint },
-          data: {
-            lastSaleDate: dayjs.unix(event.timestamp).format(),
-            lastSaleAmountSol: Number(event.amount) / LAMPORTS_PER_SOL
-          }
-        })
-        console.log(n)
-      }
+//       for (let i = data.length - 1; i >= 0; i--) {
+//         const event = data[i]
+//         const n = await prisma.nft.update({
+//           where: { mint: event.nfts[0].mint },
+//           data: {
+//             lastSaleDate: dayjs.unix(event.timestamp).format(),
+//             lastSaleAmountSol: Number(event.amount) / LAMPORTS_PER_SOL
+//           }
+//         })
+//         console.log(n)
+//       }
     
-      mostRecentTxn = data[0].signature
-    } catch (err) {
-      console.error('Error fetching from helius: ', err)
-      continue
-    }
-  }
-}
+//       mostRecentTxn = data[0].signature
+//     } catch (err) {
+//       console.error('Error fetching from helius: ', err)
+//       continue
+//     }
+//   }
+// }
 
-salesBot()
+// salesBot()
 
 process.on('uncaughtException', async err => {
   console.error('Uncaught Exception: ', err)
