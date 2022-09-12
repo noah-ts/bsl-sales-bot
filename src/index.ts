e4653009549e02450bcaf53c4ae8ce41c8d95745
Noyan <noyan.alimov@gmail.com>
2022-09-12 13:08:13 +0100

    stop

--- src/index.ts
+++ src/index.ts
@@ -18,53 +18,53 @@ let mostRecentTxn = ''
 
 console.log('Server started')
 
-const salesBot = async () => {
-  while (true) {
-    try {
-      // 5 minutes
-      await setTimeout(300000)
-      const res = await fetch(`https://api.helius.xyz/v0/addresses/${address}/nft-events?type=NFT_SALE&api-key=${API_KEY}&until=${mostRecentTxn}`)
-      const data = await res.json() as HeliusNftEventResponseType[]
-      if (!data.length) continue
+// const salesBot = async () => {
+//   while (true) {
+//     try {
+//       // 5 minutes
+//       await setTimeout(300000)
+//       const res = await fetch(`https://api.helius.xyz/v0/addresses/${address}/nft-events?type=NFT_SALE&api-key=${API_KEY}&until=${mostRecentTxn}`)
+//       const data = await res.json() as HeliusNftEventResponseType[]
+//       if (!data.length) continue
     
-      for (let i = data.length - 1; i >= 0; i--) {
-        const event = data[i]
-        let user: User
+//       for (let i = data.length - 1; i >= 0; i--) {
+//         const event = data[i]
+//         let user: User
 
-        try {
-          user = await prisma.user.findFirstOrThrow({ where: { walletAddress: event.buyer } })
-        } catch (error) {
-          if (error instanceof NotFoundError) {
-            user = await prisma.user.create({
-              data: { walletAddress: event.buyer }
-            })
-          } else {
-            console.error(error)
-            continue
-          }
-        }
+//         try {
+//           user = await prisma.user.findFirstOrThrow({ where: { walletAddress: event.buyer } })
+//         } catch (error) {
+//           if (error instanceof NotFoundError) {
+//             user = await prisma.user.create({
+//               data: { walletAddress: event.buyer }
+//             })
+//           } else {
+//             console.error(error)
+//             continue
+//           }
+//         }
 
-        const n = await prisma.nft.update({
-          where: { mint: event.nfts[0].mint },
-          data: {
-            ownerWalletAddress: user.walletAddress,
-            lastSaleDate: dayjs.unix(event.timestamp).format(),
-            lastSaleAmountSol: Number(event.amount) / LAMPORTS_PER_SOL,
-            isStaked: false
-          }
-        })
-        console.log(n)
-      }
+//         const n = await prisma.nft.update({
+//           where: { mint: event.nfts[0].mint },
+//           data: {
+//             ownerWalletAddress: user.walletAddress,
+//             lastSaleDate: dayjs.unix(event.timestamp).format(),
+//             lastSaleAmountSol: Number(event.amount) / LAMPORTS_PER_SOL,
+//             isStaked: false
+//           }
+//         })
+//         console.log(n)
+//       }
     
-      mostRecentTxn = data[0].signature
-    } catch (err) {
-      console.error(err)
-      continue
-    }
-  }
-}
+//       mostRecentTxn = data[0].signature
+//     } catch (err) {
+//       console.error(err)
+//       continue
+//     }
+//   }
+// }
 
-salesBot()
+// salesBot()
 
 process.on('uncaughtException', async err => {
   console.error('Uncaught Exception: ', err)
